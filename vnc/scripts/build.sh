3bb9c72b46b54fbb3159e07e5bf09c1fa4408d1f
mikecai <shoppernike@gmail.com>
2019-08-22 09:25:20 -0700

    Updated VNC image with updated LXDE image tag.  Also made update to scripts for new tags.

--- vnc/scripts/build.sh
+++ vnc/scripts/build.sh
@@ -2,4 +2,4 @@
 
 WORKDIR="$( cd -L "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-docker build --rm -t apcai/devbox-vnc:ubuntu-16.04 $WORKDIR
+docker build --rm -t apcai/devbox-vnc:ubuntu-16.04-lxde $WORKDIR
--- vnc/scripts/push.sh
+++ vnc/scripts/push.sh
@@ -2,5 +2,5 @@
 
 WORKDIR="$( cd -L "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-docker push apcai/devbox-vnc:ubuntu-16.04
+docker push apcai/devbox-vnc:ubuntu-16.04-lxde
 
